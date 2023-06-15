5225c2960c081648b071f595c38656d8f1fab3f6
Jungwon  Kim <kimwonny8@gmail.com>
2023-06-15 20:39:50 +0900

    project start!

---
+++ client/vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = {
+  // devServer: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8100',
+  //       changeOrigin: true,
+  //     }
+  //   }
+  // },
+  lintOnSave: false
+};
