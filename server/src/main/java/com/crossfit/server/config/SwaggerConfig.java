2a18a627d32bca56bcb58eb4e344f1f0e7bde16e
Jungwon  Kim <kimwonny8@gmail.com>
2023-07-12 22:09:20 +0900

    Test: #7 테스트 환경 구성, 테스트 코드 작성
    - 추상 클래스 생성 후 상속 받도록 테스트 클래스 구현
    - IndexController 는 @WebMvcTest 로 테스트
    - H2 데이터베이스 연결, 초기 데이터 설정
    - swagger 페이지 안열리는 오류 수정

--- server/src/main/java/com/crossfit/server/config/SwaggerConfig.java
+++ server/src/main/java/com/crossfit/server/config/SwaggerConfig.java
@@ -2,27 +2,30 @@ package com.crossfit.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
-
     @Bean
-    public OpenAPI openApi() {
-        return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
     }
 
-    private Info apiInfo() {
-        return new Info()
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
                 .version("1.0.0")
                 .title("CFKoreaMap API")
-                .description("CFKoreaMap API Document");
-
+                .description("CFKoreaMap API Document")
+                .build();
     }
-
 }
