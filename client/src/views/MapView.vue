9bc1e8337372f5bd6c63575dda026e73161b20d8
Jungwon  Kim <kimwonny8@gmail.com>
2023-07-16 19:16:24 +0900

    Feat: #6 카카오 지도 API
    - 전체 위치에서 크로스핏 센터 검색
    - 검색 기능
    - 검색 목록 표시

---
+++ client/src/views/MapView.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <KakaoMap />
+    </section>
+</template>
+<script>
+// import axios from 'axios';
+import KakaoMap from '@/components/KakaoMap.vue';
+
+export default {
+  data() {
+    return {
+        map:null
+    }
+  },
+  components: {
+    KakaoMap
+  }
+}
+</script>
+<style scoped>
+section {
+  height: 80vh;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
